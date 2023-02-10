53109306e81d119f9316b5ff8fdada2720e06162
Skryseten <skryseth@gmail.com>
2023-02-10 07:31:23 +0100

    Add project files.

---
+++ assignment-rpg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignment_rpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
