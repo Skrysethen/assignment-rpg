46b4a399c7a9ee9f4d0d8deb690c79c7f474806e
Skryseten <skryseth@gmail.com>
2023-02-17 18:36:29 +0100

    chore: Removed fluff in program.cs

--- assignment-rpg/Program.cs
+++ assignment-rpg/Program.cs
@@ -7,22 +7,7 @@ namespace assignment_rpg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            MageHero mage = new MageHero("Petter");
-            WarriorHero warrior = new WarriorHero("Conan");
-            RogueHero rogue = new RogueHero("Bilbo");
-            Console.WriteLine(mage.LevelAttributes.Intelligence);
-            WeaponItem newWeapon = new WeaponItem("common axe", 1, Slot.Weapon, WeponType.Staff, 1);
-            HeroAttribute armorModifier = new HeroAttribute { Str = 0, Dex = 0, Intelligence = 5 };
-            ArmorItem pointyHat = new ArmorItem("Wizard Hat", 1, Slot.Head, ArmorType.Cloth, armorModifier);
-            mage.Equip(pointyHat);
-            mage.Equip(newWeapon);
-            Console.WriteLine(mage.Damage());
-            Console.WriteLine(warrior.Damage());
-            Console.WriteLine(mage.ToString());
-            Console.WriteLine(rogue.ToString());
-            
-                
+                   
         }
     }
 }
